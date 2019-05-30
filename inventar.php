283faab633772c9f5f848e93d28ef254d7416826
Igor Vujkovic <igor.vujkovic@gmail.com>
2019-05-30 11:58:38 +0200

    adding css in inventar.php page

--- inventar.php
+++ inventar.php
@@ -1,15 +1,29 @@
-<?php
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inventar</title>
 </head>
+<style>
+    form {
+        width: 50%;
+        min-width: 300px;
+        border: solid 2px #999;
+        padding: 2em;
+        margin: auto;
+    }
+    input {
+        padding: 1em;
+        width: 100%;
+        display: block;
+        box-sizing: border-box;
+        font-size: 16px;
+    }
+</style>
 <body>
-
-	<div class="forma">
-		<form method="POST" action="logika/napravi_inventar.php">
+    <form method="POST" action="logika/napravi_inventar.php">
         <input type="text" name="naziv_predmeta"
             placeholder="Unesite naziv predmeta"><br>
         <input type="text" name="opis_predmeta"
@@ -18,22 +32,14 @@
             placeholder="Unesite inventarski broj"><br>
         <input type="text" name="kategorija"
             placeholder="Ponovite kategoriju"><br>
-      
 
-        
-<input type="file"
-       id="avatar" name="avatar"> <br/>
-      
+        <input type="file"
+            id="avatar" name="avatar"> <br/>
 
-        <input type="submit" value= "Napravi inventar"><br>
         <input type="radio" name="ispravno" value="true"> Ispravno <br/>
         <input type="radio" name="ispravno" value="false"> Neispravno <br/>
-
+        <input type="submit" value= "Napravi inventar"><br>
         <?php if(isset($_GET['error'])) { ?>
             <p id="error"><?php $_GET['error'] ?></p>  
         <?php } ?>
     </form>
-	</div>
-	<div class="lista_predmeta"></div>
-</body>
-</html> 
